7eabf036a6b231e37460168206166b76458c1b64
Adam btw <adam.tait617@gmail.com>
2026-02-12 16:03:37 +0000

    create macro file if none exist

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
 )
 
 // set path for the csv file containing macros
-var macroFile = ".config/launchpad/commands.csv"
+var macroDir = ".config/launchpad/"
+var macroFile = "commands.csv"
 
 // set 'amidi' as the linux command to use for communicating with the launchpad
 var lpCmd string = "amidi"
@@ -28,12 +30,10 @@ const defaultColor = amber
 var colors = map[string]int{"green": green, "red": red, "amber": amber, "lime": lime}
 
 func main() {
-	// set config path
-	dir, err := os.UserHomeDir()
-	macroFile = dir + "/" + macroFile
-	// change to home dir
-	os.Chdir(dir)
-
+	// setup config
+	if err := setConfig(); err != nil {
+		log.Fatalf("Error setting up config: %v", err)
+	}
 	// get the launchpad struct
 	fmt.Println("Getting launchpad...")
 	lp, err := getLaunchpad()
@@ -46,4 +46,29 @@ func main() {
 		log.Fatalf("Error starting launchpad: %v", err)
 	}
 
+}
+
+// function to find / create macro command config file
+func setConfig() error {
+	// set config path
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("Error finding user home dir: %v", err)
+	}
+	macroFile = homeDir + "/" + macroDir + macroFile
+
+	// create the file
+	if _, err := os.Stat(macroFile); errors.Is(err, os.ErrNotExist) {
+		if err := os.Mkdir(homeDir+"/"+macroDir, 0777); err != nil {
+			return fmt.Errorf("Error creating config directory: %v", err)
+		}
+		f, err := os.Create(macroFile)
+		if err != nil {
+			return fmt.Errorf("Error creating config file: %v", err)
+		}
+		f.Close()
+	}
+
+	// change to home dir
+	return os.Chdir(homeDir)
 }
